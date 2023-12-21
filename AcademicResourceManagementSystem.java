79eb21dbd22751e5606fa15e75272a492eeaf6f2
Dawit-asefa <adawit473@gmail.com>
2023-12-21 22:19:54 +0300

    the second starting code

--- AcademicResourceManagementSystem.java
+++ AcademicResourceManagementSystem.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.List;
 public class AcademicResourceManagementSystem {
 
 }
