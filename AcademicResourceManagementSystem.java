e0f135ac34bf07416c795b60b8601a29f9dadd86
Dawit-asefa <adawit473@gmail.com>
2023-12-22 23:23:13 +0300

    adding some that should available in the project

--- AcademicResourceManagementSystem.java
+++ AcademicResourceManagementSystem.java
@@ -1,12 +1,11 @@
 
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
+
 public class AcademicResourceManagementSystem {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/university_system";
     // private static final String DB_USER = "root";
@@ -18,6 +17,7 @@ public class AcademicResourceManagementSystem {
         String username = null;  // Declare the username variable outside the block
 
         try {
+            //connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
             System.out.println("Enter database username:");
             String dbUser = scanner.nextLine();
 
@@ -41,11 +41,9 @@ public class AcademicResourceManagementSystem {
                         username = scanner.next();
                         System.out.println("Enter your password:");
                         String password = scanner.next();
-                        registerStudent(connection, username, password);
+
                         System.out.println("Registration successful!");
                         break;
-
-
 
                     case 2:
                         // See Schedule for Student
@@ -55,39 +53,89 @@ public class AcademicResourceManagementSystem {
                         switch (scheduleType) {
                             case 1:
                                 // Display Class Schedule
-                                viewClassScheduleForStudent(connection);
+
                                 break;
 
                             case 2:
                                 // Display Assignment Schedule (implement logic)
+
                                 System.out.println("Assignment Schedule functionality is not implemented yet.");
                                 break;
 
+                            case 3:
+                                // Display Exam Schedule
 
+                                break;
 
+                            default:
+                                System.out.println("Invalid choice");
+                        }
+                        break;
+
+                    case 3:
+                        // See Grade
+                        System.out.println("Enter your username:");
+                        username = scanner.next();
+
+                        break;
+
+                    case 4:
+                        // See Material
+
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice");
                 }
-
-            }
-
-
-
-            else if ("teacher".equalsIgnoreCase(userType)) {
+            } else if ("teacher".equalsIgnoreCase(userType)) {
                 // Teacher Functionality
                 System.out.println("1. Upload Schedule\n  1. Upload Class Schedule\n  2. Upload Assignment Schedule\n  3. Upload Exam Schedule\n2. Upload Grade\n3. Upload Material\nEnter your choice:");
                 int teacherChoice = scanner.nextInt();
 
-            }
+
+                switch (teacherChoice) {
+                    case 1:
+                        // Upload Schedule
+                        System.out.println("1. Upload Class Schedule\n  2. Upload Assignment Schedule\n  3. Upload Exam Schedule\nEnter your choice:");
+                        int scheduleChoice = scanner.nextInt();
+
+                        switch (scheduleChoice) {
+                            case 1:
+                                // Upload Class Schedule
+
+                                break;
+
+                            case 2:
+                                // Upload Assignment Schedule
+
+                                break;
+
+                            case 3:
+                                // Upload Exam Schedule
+
+                                break;
 
 
-            else {
+                            default:
+                                System.out.println("Invalid choice");
+                        }
+                        break;
+
+                    case 2:
+                        // Upload Grade
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice");
+                }
+            } else {
                 System.out.println("Invalid user type");
             }
-
-
-
-
-
-
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Database connection error");
@@ -97,21 +145,5 @@ public class AcademicResourceManagementSystem {
                 scanner.close();
             }
         }
-
-
-
-
     }
-    private static void registerStudent(Connection connection, String username, String password) throws SQLException {
-        String insertUserQuery = "INSERT INTO users (username, password, role) VALUES (?, ?, 'student')";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(insertUserQuery)) {
-            preparedStatement.setString(1, username);
-            preparedStatement.setString(2, password);
-            preparedStatement.executeUpdate();
-            System.out.println("Student registration successful!");
-        }
-    }
-
-
-
 }
