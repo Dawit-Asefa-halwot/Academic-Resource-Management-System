4682c57f77a45ea8fd17c6876c5f3adc58aaacc3
Dawit-asefa <adawit473@gmail.com>
2023-12-22 17:45:22 +0300

    databse connection

--- AcademicResourceManagementSystem.java
+++ AcademicResourceManagementSystem.java
@@ -8,5 +8,14 @@ import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.List;
 public class AcademicResourceManagementSystem {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/university_system";
+    // private static final String DB_USER = "root";
+    // private static final String DB_PASSWORD = "1234";
+    private static Connection connection;
+    private static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        // Declare the username variable outside the block
+
+    }
 }
